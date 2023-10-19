c344ea5e06ecfe885bae901f631c6fc9030d7117
clvalada <clvalada@gmail.com>
2023-10-19 16:04:14 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -14,81 +14,95 @@ inquirer
     },
     {
       type: 'input',
-      message: 'Please provide a summary description of your application.',
+      message: 'Please provide a short description of your application explaining the what, why, and how of your project.',
       name: 'description',
     },
     {
-      type: 'input',
-      message: 'Please enter your GitHub username:',
-      name: 'github',
+        type: 'input',
+        message: 'Please provide installation instructions.',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Please provide usage instructions.',
+        name: 'usage',
+      },
+    {
+      type: 'list',
+      message: 'Please chose a license type.',
+      name: 'license',
+      choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause','unlicense','Other'],
     },
+    {
+        type: 'input',
+        message: 'Please list any references, sources, or collaborators.',
+        name: 'credits',
+      },
+      {
+        type: 'input',
+        message: 'Please provide instructions for contributing',
+        name: 'contributing',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your GitHub username',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your email address',
+        name: 'email',
+      },
   ])
 // Function to write README file  
   .then((response) => {
       const ReadMeTemplate = `
-      # <Your-Project-Title>
+# ${response.appname}
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+${response.description}
 
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
 - [License](#license)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+${response.installation}
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
+${response.usage}
 
 ## License
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+${response.license}
 
----
+## Credits
 
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+${response.credits}
 
-## Badges
+## Contributing
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-
-## Features
-
-If your project has a lot of features, list them here.
-
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+${response.contributing}
 
 ## Tests
 
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+${response.tests}
+
+## Questions
+
+If you have any questions, please feel free to contact me at ${response.email}. 
+
+Alternatively, you can view my other projects at github.com/${response.username}
+
         `;
         fs.writeFile('samplereadme.md', ReadMeTemplate, (err) => {
             if (err) {
